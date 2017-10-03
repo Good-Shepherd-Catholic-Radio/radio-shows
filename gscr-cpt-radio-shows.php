b9fde09d40a4fd42e60db8ee76eb07a80069312a
Eric Defore <eric.defore@gmail.com>
2017-10-03 12:29:25 -0400

    GH Header

--- gscr-cpt-radio-shows.php
+++ gscr-cpt-radio-shows.php
@@ -9,7 +9,7 @@
  * Author URI: http://realbigmarketing.com/
  * Contributors: d4mation
  * GitHub Plugin URI: Good-Shepherd-Catholic-Radio/cpt-radio-shows
- * GitHub Branch: develop
+ * GitHub Branch: master
  */
 
 // Exit if accessed directly
