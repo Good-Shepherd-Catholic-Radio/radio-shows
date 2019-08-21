0d78fdaa26d4f9dcd93a4d96b4552f8a79d0d271
Eric Defore <eric.defore@gmail.com>
2019-08-21 13:18:22 -0400

    Ensure that deleting Radio Shows works as expected

--- build/js/admin/radio-show-delete.js
+++ build/js/admin/radio-show-delete.js
@@ -6,16 +6,16 @@
 
         $repeater.repeater.on( 'repeater-before-delete-item', function( event, $item ) {
 
-            let post_id = parseInt( $item.find( '[data-fieldhelpers-name="post_id"]' ).val() );
+            let post_id = parseInt( $item.find( '[data-fieldhelpers-name="post_id"] input' ).val() );
 
-            if ( post_id.length > 0 && post_id !== 0 ) {
+            if ( post_id > 0 ) {
 
-                let post_ids = $( '[name="radio_show_occurrences_to_delete"]' ).val().trim();
+                let post_ids = $( '[name="rbm_cpts_radio_show_occurrences_to_delete"]' ).val().trim();
 
-                post_ids = post_ids.split( ',' );
+                post_ids = post_ids.split( ',' ).filter( Boolean );
                 post_ids.push( post_id );
 
-                $( '[name="radio_show_occurrences_to_delete"]' ).val( post_ids.join( ',' ) );
+                $( '[name="rbm_cpts_radio_show_occurrences_to_delete"]' ).val( post_ids.join( ',' ) );
 
             }
 
